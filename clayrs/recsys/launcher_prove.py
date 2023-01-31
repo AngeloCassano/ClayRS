a759d8dc033e90a89874b022e0849d7fafcf4063
Angelo Cassano <angel96ka@gmail.com>
2023-01-31 18:05:57 +0100

    corretto nome modulo explanation_generator e aggiunto come commento la prossima funzionalità da implementare con la funzione generate_explanation

--- clayrs/recsys/launcher_prove.py
+++ clayrs/recsys/launcher_prove.py
@@ -1,7 +1,11 @@
 import explanation
-import explaination_generator
+import explanation_generator
 from clayrs.recsys import NXFullGraph, ItemNode, PropertyNode, NXPageRank, TestRatingsMethodology
+#from clayrs.recsys.Generator import generate_explanation
 
-G, sorted_prop, profile, recommendations= explanation.explain(["I:11033","I:8360","I:1661","I:8487"], ["I:11768","I:69"])
-triple_structure = explaination_generator.build_triple_structure(G, sorted_prop, profile, recommendations)
+G, sorted_prop, profile, recommendations= explanation.explain(["I:11033","I:8360","I:1661","I:8487"], ["I:11768"]) #,"I:69"
+triple_structure = explanation_generator.build_triple_structure(G, sorted_prop, profile, recommendations)
 print(triple_structure)
+print()
+#explanation = generate_explanation(triple_structure, recommendations, profile)
+#print(explanation)
